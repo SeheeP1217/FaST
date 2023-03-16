e34605da74ccae4f4ec33591b7741cd32b2b3ba5
Oh JUN Ho <dhwnsghdhkd@naver.com>
2023-03-17 00:58:29 +0900

    [BE] feat : 소셜 로그인 성공

---
+++ backend/src/main/java/a402/FaST/Controller/OauthController.java
@@ -0,0 +1,66 @@
+package a402.FaST.Controller;
+
+import a402.FaST.exception.DuplicateMemberException;
+import a402.FaST.model.dto.AuthorityDto;
+import a402.FaST.model.dto.TokenDto;
+import a402.FaST.model.dto.UserResponseDto;
+import a402.FaST.model.entity.User;
+import a402.FaST.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@CrossOrigin
+@RestController
+@RequiredArgsConstructor
+public class OauthController {
+    private static final Logger logger = LoggerFactory.getLogger(OauthController.class);
+    private final UserRepository userRepository;
+
+    @GetMapping("/success")
+    private ResponseEntity<Map<String, Object>> getUser(@RequestParam("code") String code, @RequestParam("email") String email ) {
+
+        Map<String, Object> resultMap = new HashMap<>();
+        HttpStatus status = HttpStatus.OK;
+
+        logger.info("code {}",code);
+        logger.info("email {}",email);
+        User user = userRepository.findByEmail(email).get();
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new DuplicateMemberException("이미 가입되어 있는 유저입니다.");
+        }
+
+
+        TokenDto TokenResponseDto = null;
+        UserResponseDto userResponseDto = null;
+
+        TokenResponseDto = TokenDto.builder()
+                .token(code)
+                .build();
+        userResponseDto = UserResponseDto.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .nickname(user.getNickname())
+//                .salt(user.getSalt())
+                .imgPath(user.getImg_path())
+                .authorityDtoSet(user.getAuthorities().stream()
+                        .map(authority -> AuthorityDto.builder().authorityName(authority.getAuthorityName()).build())
+                        .collect(Collectors.toSet()))
+                .build();
+
+        resultMap.put("token", TokenResponseDto);
+        resultMap.put("user", userResponseDto);
+
+
+        return new ResponseEntity<Map<String, Object>>(resultMap, status);
+    }
+
+
+}
--- backend/src/main/java/a402/FaST/handler/OAuth2AuthenticationSuccessHandler.java
+++ backend/src/main/java/a402/FaST/handler/OAuth2AuthenticationSuccessHandler.java
@@ -39,95 +39,33 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
 
         // redirect 할 url을 지정해준다
-        String targetUrl = "/";
-        String jwt = tokenProvider.createToken(authentication);
+        String targetUrl = determineTargetUrl(request,response,authentication);
 
         if (response.isCommitted()) {
             logger.debug("Response has already been committed. Unable to redirect to " + targetUrl);
             return;
         }
 
+//        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+//        String email = principalDetails.getUser().getEmail();
+//        response.sendRedirect("/success?code=" + ((PrincipalDetails) authentication.getPrincipal()).getUser().getEmail());
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
+
+
+    protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        String targetUrl = "/success";
+
+        String jwt = tokenProvider.createToken(authentication);
         PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
         String email = principalDetails.getUser().getEmail();
-        System.out.println(email);
-        TokenDto TokenResponseDto = null;
-        UserResponseDto userResponseDto = null;
-        HttpStatus status = HttpStatus.OK;
-        User user = userRepository.findByEmail(email).get();
-        System.out.println(user.getNickname());
 
-        Map<String, Object> resultMap = new HashMap<>();
 
-        TokenResponseDto = TokenDto.builder()
-                .token(jwt)
-                .build();
-        userResponseDto = UserResponseDto.builder()
-                .id(user.getId())
-                .email(user.getEmail())
-                .nickname(user.getNickname())
-//                .salt(user.getSalt())
-                .imgPath(user.getImg_path())
-                .authorityDtoSet(user.getAuthorities().stream()
-                        .map(authority -> AuthorityDto.builder().authorityName(authority.getAuthorityName()).build())
-                        .collect(Collectors.toSet()))
-                .build();
-
-        resultMap.put("token", TokenResponseDto);
-        resultMap.put("user", userResponseDto);
-
-        System.out.println(resultMap);
-        ObjectMapper objectMapper = new ObjectMapper();
-        String jsonData = objectMapper.writeValueAsString(resultMap);
-        PrintWriter writer = response.getWriter();
-        writer.println(jsonData);
-
-//        return;
-
-//        response.setContentType("text/html;charset=UTF-8");
-//        response.setContentType("application/json");
-        moveurl(resultMap);
-//        response.sendRedirect(targetUrl);
-//        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+        return UriComponentsBuilder.fromUriString(targetUrl)
+                .queryParam("code", jwt)
+                .queryParam("email",email)
+                .build().toUriString();
     }
-
-    private ResponseEntity<Map<String, Object>> moveurl(Map<String, Object> resultMap) {
-        HttpStatus status = HttpStatus.OK;
-        return new ResponseEntity<Map<String, Object>>(resultMap, status);
-    }
-
-//    protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-//
-////        String targetUrl = "http://localhost:3000/home";
-//        String targetUrl = "/";
-////        String targetUrl = "/oauth2/social/success";
-//
-//        // 3. 인증 정보를 기반으로 JWT 토큰 생성
-//        String jwt = tokenProvider.createToken(authentication);
-////        System.out.println(jwt);
-//
-////        // 4. RefreshToken 저장
-////        RefreshToken refreshToken = RefreshToken.builder()
-////                .key(authentication.getName())
-////                .value(tokenDto.getRefreshToken())
-////                .build();
-////
-////        refreshTokenRepository.save(refreshToken);
-////
-////        int cookieMaxAge = tokenProvider.getExpiration().intValue() / 60;
-////
-////        CookieUtil.deleteCookie(request, response, "refreshToken");
-////        CookieUtil.addCookie(response, "refreshToken", tokenDto.getRefreshToken(), cookieMaxAge);
-////
-////        CookieUtil.deleteCookie(request, response, "grantType");
-////        CookieUtil.addCookie(response, "grantType", tokenDto.getGrantType(), cookieMaxAge);
-////
-////        CookieUtil.deleteCookie(request, response, "accessTokenExpiresIn");
-////        CookieUtil.addCookie(response, "accessTokenExpiresIn", Long.toString(tokenDto.getAccessTokenExpiresIn()), cookieMaxAge);
-//
-//        return UriComponentsBuilder.fromUriString(targetUrl)
-//                .queryParam("accessToken", jwt)
-//                .build().toUriString();
-//    }
 
 
 }
